96f5ce3343628b2dd06d5ff4686d1ecd891ffe1c
Andrew Yuen <andrewcy86@users.noreply.github.com>
2019-04-16 22:50:19 -0400

    Update utils.py

--- nlp/utils.py
+++ nlp/utils.py
@@ -111,7 +111,7 @@ def analyze_text(text):
         #Summarize the document with 2 sentences
         summary = summarizer(parser.text, 2)
         for sentence in summary:
-        ret['summary'] = print(sentence)
+            ret['summary'] = print(sentence)
     except ValueError:  # why does it break in short sentences?
         ret['summary'] = ''
 
