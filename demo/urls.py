0ca4e87ab6466bbd9d5caacaf86e0ffd6a28ba06
Andrew Yuen <andrewcy86@users.noreply.github.com>
2019-04-17 05:26:11 -0700

    Update urls.py
    Changed for localhost testing.

--- demo/urls.py
+++ demo/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^', include('nlp.urls')),
-#    url(r'^admin/', admin.site.urls),
-    ]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
