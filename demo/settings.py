79915b73279b27eb75841302475da6273741667d
Andrew Yuen <andrewcy86@users.noreply.github.com>
2019-07-22 16:58:34 -0400

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL='/media/'
-STATIC_ROOT = BASE_DIR + '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,  'static')]
 MEDIA_ROOT = BASE_DIR + '/media/'
 
 
